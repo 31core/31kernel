de66ea288df4da64754fc034beb56940dcee9690
31core <31core@mail.com>
2026-03-01 15:20:25 +0800

    Organize `kernel_main` function

--- src/arch.rs
+++ src/arch.rs
@@ -1,2 +1,14 @@
+/*!
+ *  Architecture related code
+ */
+
 #[cfg(target_arch = "riscv64")]
 pub mod riscv64;
+
+use core::arch::global_asm;
+
+#[cfg(target_arch = "riscv64")]
+global_asm!(include_str!("arch/riscv64/entry.S"));
+
+#[cfg(target_arch = "aarch64")]
+global_asm!(include_str!("arch/arm64/entry.S"));
